3cd916526b0b3d422694e21c303e05f11f1b2cb4
mishu <mishu@xtelecom.ro>
2019-10-05 09:41:32 +0300

    fixes stocks filters & deps

--- src/bulma/pages/audit/Index.vue
+++ src/bulma/pages/audit/Index.vue
@@ -15,17 +15,10 @@
             <div class="column is-3-desktop">
                 <enso-select-filter class="box raises-on-hover"
                     source="products.options"
-                    disable-filtering
                     :title="i18n('Product')"
                     v-model="filters.client_stocks.product_id"/>
             </div>
         </div>
-        <filter-state :api-version="apiVersion"
-            name="stocks_filters"
-            :params="params"
-            :filters="filters"
-            @ready="ready = true"
-            ref="filterState"/>
         <div class="has-margin-bottom-large" v-if="params.client !== 'person'">
             <p class="title is-5 has-text-centered">
                 {{ i18n('Client Stocks') }}
@@ -36,39 +29,22 @@
                 :filters="stockFilters"
                 @import="openUploader"
                 @reset="$refs.filterState.reset()"/>
-
             <import-uploader v-show="false"
                 :path="route('import.store')"
                 :params="uploaderParams"
-                v-if="stockFilters.client_stocks.client_id"
+                v-if="filters.client_stocks.client_id"
                 ref="uploader"/>
         </div>
         <div class="columns is-centered has-margin-top-large"
-            v-if="ready">
+             v-if="ready">
             <div class="column is-narrow">
                 <enso-date-filter class="box raises-on-hover"
-                    v-model="saleDateParams.dateInterval"
+                    v-model="params.sales.dateInterval"
                     default="thirtyDays"
                     :title="i18n('Date')"
-                    :interval="saleIntervals.sales.date"
-                    @update="
-                        saleIntervals.sales.date.min = $event.min;
-                        saleIntervals.sales.date.max = $event.max;
-                        saleReturnIntervals.sale_returns.date.min = $event.min;
-                        saleReturnIntervals.sale_returns.date.max = $event.max;
-                    "/>
+                    :interval="intervals.sales.date"/>
             </div>
         </div>
-        <filter-state :api-version="apiVersion"
-            name="sale_intervals"
-            :params="saleDateParams"
-            :intervals="saleIntervals"
-            ref="saleIntervalsState"/>
-        <filter-state :api-version="apiVersion"
-            name="sale_return_intervals"
-            :params="saleDateParams"
-            :intervals="saleReturnIntervals"
-            ref="saleReturnIntervalsState"/>
         <div class="columns">
             <div class="column">
                 <p class="title is-5 has-text-centered">
@@ -77,57 +53,32 @@
                 <enso-table class="box is-paddingless raises-on-hover"
                     id="stock-sales"
                     :path="route('commercial.audit.sales.initTable')"
-                    :filters="productFilter('sale_lines')"
+                    :filters="productFilter"
                     :params="saleParams"
-                    :intervals="saleIntervals"
-                    @reset="
-                        $refs.saleIntervalsState.reset();
-                        $refs.saleReturnIntervalsState.reset();
-                    "/>
+                    @reset="$refs.saleIntervalsState.reset();"/>
             </div>
             <div class="column">
                 <p class="title is-5 has-text-centered">
                     {{ i18n('Sale Returns') }}
                 </p>
                 <enso-table class="box is-paddingless raises-on-hover"
-                    id="stocks-saleReturns"
+                    id="stocks-sale-returns"
                     :path="route('commercial.audit.saleReturns.initTable')"
-                    :filters="productFilter('sale_return_lines')"
+                    :filters="productFilter"
                     :params="saleParams"
-                    :intervals="saleReturnIntervals"
-                    @reset="
-                        $refs.saleIntervalsState.reset();
-                        $refs.saleReturnIntervalsState.reset();
-                    "/>
+                    @reset="$refs.saleIntervalsState.reset();"/>
             </div>
         </div>
-
         <div class="columns is-centered"
-            v-if="ready">
+             v-if="ready">
             <div class="column is-narrow">
                 <enso-date-filter class="box raises-on-hover"
-                    v-model="purchaseDateParams.dateInterval"
+                    v-model="params.purchases.dateInterval"
                     default="thirtyDays"
                     :title="i18n('Date')"
-                    :interval="purchaseIntervals.purchases.date"
-                    @update="
-                        purchaseIntervals.purchases.date.min = $event.min;
-                        purchaseIntervals.purchases.date.max = $event.max;
-                        purchaseReturnIntervals.purchase_returns.date.min = $event.min;
-                        purchaseReturnIntervals.purchase_returns.date.max = $event.max;
-                    "/>
+                    :interval="intervals.purchases.date"/>
             </div>
         </div>
-        <filter-state :api-version="apiVersion"
-            name="purchase_intervals"
-            :params="purchaseDateParams"
-            :intervals="purchaseIntervals"
-            ref="purchaseIntervalsState"/>
-        <filter-state :api-version="apiVersion"
-            name="purchase_return_intervals"
-            :params="purchaseDateParams"
-            :intervals="purchaseReturnIntervals"
-            ref="purchaseReturnIntervalsState"/>
         <div class="columns">
             <div class="column">
                 <p class="title is-5 has-text-centered has-margin-top-large">
@@ -136,30 +87,27 @@
                 <enso-table class="box is-paddingless raises-on-hover"
                     id="stocks-purchases"
                     :path="route('commercial.audit.purchases.initTable')"
-                    :filters="productFilter('purchase_lines')"
-                    :intervals="purchaseIntervals"
-                    :params="purchaseParams"
-                    @reset="
-                        $refs.purchaseIntervalsState.reset();
-                        $refs.purchaseReturnIntervalsState.reset();
-                    "/>
+                    :filters="productFilter"
+                    :params="purchaseParams"/>
             </div>
             <div class="column">
                 <p class="title is-5 has-text-centered has-margin-top-large">
                     {{ i18n('Purchase Returns') }}
                 </p>
                 <enso-table class="box is-paddingless raises-on-hover"
-                    id="stocks-purchaseReturns"
+                    id="stocks-purchase-returns"
                     :path="route('commercial.audit.purchaseReturns.initTable')"
-                    :filters="productFilter('purchase_return_lines')"
-                    :intervals="purchaseReturnIntervals"
-                    :params="purchaseParams"
-                    @reset="
-                        $refs.purchaseIntervalsState.reset();
-                        $refs.purchaseReturnIntervalsState.reset();
-                    "/>
+                    :filters="productFilter"
+                    :params="purchaseParams"/>
             </div>
         </div>
+        <filter-state :api-version="apiVersion"
+            name="intervals"
+            :params="params"
+            :filters="filters"
+            :intervals="intervals"
+            @ready="ready = true"
+            ref="intervalsState"/>
     </div>
 </template>
 
@@ -185,7 +133,7 @@ export default {
 
     data() {
         return {
-            apiVersion: 2,
+            apiVersion: 1.3,
             ready: false,
             filters: {
                 client_stocks: {
@@ -195,7 +143,7 @@ export default {
                     product_id: null,
                 },
             },
-            saleIntervals: {
+            intervals: {
                 sales: {
                     date: {
                         min: null,
@@ -203,27 +151,7 @@ export default {
                         dbDateFormat: 'Y-m-d',
                     },
                 },
-            },
-            saleReturnIntervals: {
-                sale_returns: {
-                    date: {
-                        min: null,
-                        max: null,
-                        dbDateFormat: 'Y-m-d',
-                    },
-                },
-            },
-            purchaseIntervals: {
                 purchases: {
-                    date: {
-                        min: null,
-                        max: null,
-                        dbDateFormat: 'Y-m-d',
-                    },
-                },
-            },
-            purchaseReturnIntervals: {
-                purchase_returns: {
                     date: {
                         min: null,
                         max: null,
@@ -233,44 +161,18 @@ export default {
             },
             params: {
                 client: null,
-            },
-            saleDateParams: {
-                dateInterval: 'thirtyDays',
-            },
-            purchaseDateParams: {
-                dateInterval: 'thirtyDays',
+                sales: {
+                    dateInterval: 'thirtyDays',
+                },
+                purchases: {
+                    dateInterval: 'thirtyDays',
+                },
+
             },
         };
     },
 
     computed: {
-        uploaderParams() {
-            return {
-                type: 'clientStocks',
-                client: this.stockFilters.client_stocks.client_id,
-            };
-        },
-        saleFilters() {
-            return {
-                sales: {
-                    company_id: this.filters.client_stocks.company_id,
-                    person_id: this.filters.client_stocks.person_id,
-                    product_id: this.filters.client_stocks.product_id,
-                },
-            };
-        },
-        saleParams() {
-            return {
-                company_id: this.filters.client_stocks.company_id,
-                person_id: this.filters.client_stocks.person_id,
-                supplier_id: this.filters.client_stocks.supplier_id,
-            };
-        },
-        purchaseParams() {
-            return {
-                supplier_id: this.filters.client_stocks.supplier_id,
-            };
-        },
         stockFilters() {
             return {
                 client_stocks: {
@@ -280,20 +182,44 @@ export default {
                 },
             };
         },
+        uploaderParams() {
+            return {
+                type: 'clientStocks',
+                client: this.filters.client_stocks.client_id,
+            };
+        },
+        productFilter() {
+            return {
+                products: {
+                    id: this.filters.client_stocks.product_id,
+                },
+            };
+        },
+        saleParams() {
+            return {
+                company_id: this.filters.client_stocks.company_id,
+                person_id: this.filters.client_stocks.person_id,
+                supplier_id: this.filters.client_stocks.supplier_id,
+                startDate: this.intervals.sales.date.min,
+                endDate: this.intervals.sales.date.max,
+            };
+        },
+        purchaseParams() {
+            return {
+                supplier_id: this.filters.client_stocks.supplier_id,
+                startDate: this.intervals.purchases.date.min,
+                endDate: this.intervals.purchases.date.max,
+            };
+        },
     },
 
     methods: {
         openUploader() {
-            if (!this.stockFilters.client_stocks.client_id) {
+            if (!this.filters.client_stocks.client_id) {
                 this.$toastr.warning(this.i18n('Please select a client first'));
                 return;
             }
             this.$refs.uploader.browseFiles();
-        },
-        productFilter(table) {
-            return {
-                [table]: { product_id: this.filters.client_stocks.product_id },
-            };
         },
     },
 
